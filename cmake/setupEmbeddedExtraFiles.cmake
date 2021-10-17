5e59b068999a670dd8e72c4d1d9a0f647ba6ee10
Michael Albrecht <michael.albrecht@ovgu.de>
2021-10-18 01:50:52 +0200

    Added script that scans all static libraries for dangerous weak / strong double definition #3

--- cmake/setupEmbeddedExtraFiles.cmake
+++ cmake/setupEmbeddedExtraFiles.cmake
@@ -1,9 +1,14 @@
 include_guard(GLOBAL)
+# Adds generation of .bin and .list files
+# Makes target generate as an .elf file
+# Invokes static library analyzer see core issue #3
 # assumes CMAKE_OBJCOPY, CMAKE_OBJDUMP is correctly setup globally
-# also assumes target FirmwareName already exists (target = add_executable or add_library)
-# modifies target so that it generates with TargetName.elf filename
 
-function(SETUP_EXTRA_FILE_GENERATION TargetName)
+# TargetName: your target from add_executable
+# PathToCore: relative path from where you are calling this function to inside core folder without trailing /
+#             (e.g. ../libraries/core)
+
+function(SETUP_EXTRA_FILE_GENERATION TargetName PathToCore)
     # make it so firmware generates with .elf file ending
     set_target_properties(
             ${TargetName}
@@ -22,5 +27,11 @@ function(SETUP_EXTRA_FILE_GENERATION TargetName)
             TARGET ${TargetName}
             POST_BUILD
             COMMAND ${CMAKE_OBJDUMP} ARGS -S "${CMAKE_BINARY_DIR}/${TargetName}.elf" > "${CMAKE_BINARY_DIR}/${TargetName}.list"
+    )
+
+    add_custom_command(
+            TARGET ${TargetName}
+            POST_BUILD
+            COMMAND ${CMAKE_CURRENT_LIST_DIR}/${PathToCore}/scripts/checkStaticLibraries.py ARGS ${CMAKE_BINARY_DIR}
     )
 endfunction()
