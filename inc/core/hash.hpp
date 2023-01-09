927740d786ffc39eb83b143da308b44c9211d814
MG-5 <mg-95@t-online.de>
2023-01-09 19:02:43 +0100

    Add compile-time hash function for string_views

--- inc/core/hash.hpp
+++ inc/core/hash.hpp
@@ -1,14 +1,26 @@
 #pragma once
 
 #include <cstdint>
+#include <string_view>
 
 namespace core::hash
 {
-    static constexpr uint64_t HASH_SEED = 0xcbf29ce484222325;
-    static constexpr uint64_t MagicPrime = 0x00000100000001b3;
+static constexpr uint64_t HASH_SEED = 0xcbf29ce484222325;
+static constexpr uint64_t MagicPrime = 0x00000100000001b3;
 
-    uint64_t fnvWithSeed(uint64_t hash, const uint8_t *data, const uint8_t *const dataEnd);
+uint64_t fnvWithSeed(uint64_t hash, const uint8_t *data, const uint8_t *const dataEnd);
 
-    static constexpr uint64_t FirmwareHashTestingValue{0xDEADBEEFC0FFEE};
-    uint64_t computeFirmwareHash();
-} // namespace bus_node_base
+constexpr uint64_t fnvStringview(const std::string_view &str)
+{
+    uint64_t hash = HASH_SEED;
+    for (char c : str)
+    {
+        hash = (hash ^ c) * MagicPrime;
+    }
+
+    return hash;
+}
+
+static constexpr uint64_t FirmwareHashTestingValue{0xDEADBEEFC0FFEE};
+uint64_t computeFirmwareHash();
+} // namespace core::hash
