3cfc08b3883bbcb1b68f4fd91dd51ebe98d3d656
MG-5 <mg-95@t-online.de>
2024-04-25 23:46:13 +0200

    Remove stubs due CubeMX 6.11 define these already

--- src/abi.cpp
+++ src/abi.cpp
@@ -110,50 +110,5 @@ extern "C"
     void __errno()
     {
     }
-    
-    // GCC newer than 10 produces warnings that functions from newlibs are not implemented
-    // so we use stubs provided here https://sourceware.org/newlib/libc.html#Stubs
-
-    int _close(int file)
-    {
-        return -1;
-    }
-
-#include <sys/stat.h>
-    int _fstat(int file, struct stat *st)
-    {
-        st->st_mode = S_IFCHR;
-        return 0;
-    }
-
-    int _getpid(void)
-    {
-        return 1;
-    }
-
-    int _isatty(int file)
-    {
-        return 1;
-    }
-
-    int _kill(int pid, int sig)
-    {
-        return -1;
-    }
-
-    int _lseek(int file, int ptr, int dir)
-    {
-        return 0;
-    }
-
-    int _read(int file, char *ptr, int len)
-    {
-        return 0;
-    }
-
-    int _write(int file, char *ptr, int len)
-    {
-        return len;
-    }
 }
 #endif
